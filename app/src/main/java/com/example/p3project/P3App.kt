82f070be96dca6f06cdcc51e4b5b185291bce210
William Robertson <whr1g21@soton.ac.uk>
2024-02-26 22:12:11 +0000

    Finished creating notification channel

--- app/src/main/java/com/example/p3project/P3App.kt
+++ app/src/main/java/com/example/p3project/P3App.kt
@@ -2,7 +2,10 @@ package com.example.p3project;
 import android.app.Application;
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
 import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.p3project.common.Constants
 
 class P3App: Application() {
@@ -16,11 +19,14 @@ class P3App: Application() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is not in the Support Library.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel(Constants.NOTIFICATION_CHANNEL_ID,
-                "Notification",
-                NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(Constants.NOTIFICATION_CHANNEL_ID,
+                "Notification", NotificationManager.IMPORTANCE_DEFAULT
                 )
-        }
+            channel.description="Description"
 
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
