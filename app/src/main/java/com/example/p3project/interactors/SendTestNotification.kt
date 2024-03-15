72a6fea6c95652925f1d65322a8314212562d8be
William Robertson <whr1g21@soton.ac.uk>
2024-03-15 12:34:01 +0000

    Renamed Services to Use Case

--- app/src/main/java/com/example/p3project/interactors/SendTestNotification.kt
+++ app/src/main/java/com/example/p3project/interactors/SendTestNotification.kt
@@ -1,10 +1,10 @@
 package com.example.p3project.interactors
 
 import android.content.Context
-import com.example.p3project.sources.services.TestNotificationService
+import com.example.p3project.sources.usecases.TestNotificationUseCase
 
 class SendTestNotification(private val context: Context) {
-    private val notificationService = TestNotificationService(context)
+    private val notificationService = TestNotificationUseCase(context)
 
     operator fun invoke(text: String) {
         if (text.isNotBlank()) {
