f3da8c7fcf531064c7fd68135e4f937479258580
William Robertson <whr1g21@soton.ac.uk>
2024-03-15 12:25:07 +0000

    Created new navigation structure

--- app/src/main/java/com/example/p3project/sources/presentation/MainActivity.kt
+++ app/src/main/java/com/example/p3project/sources/presentation/MainActivity.kt
@@ -4,14 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
@@ -19,8 +12,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.p3project.sources.presentation.screens.Screen
 import com.example.p3project.sources.presentation.screens.calendar.CalendarScreen
+import com.example.p3project.sources.presentation.screens.test.TestScreen
 import com.example.p3project.sources.presentation.screens.overview.OverviewScreen
 import com.example.p3project.sources.presentation.theme.P3ProjectTheme
+import dagger.hilt.android.AndroidEntryPoint
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,32 +27,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Scaffold(bottomBar = {
-                        BottomAppBar {
-                            NavigationBarItem(
-                                selected = false,
-                                onClick = { /*TODO*/ },
-                                icon = { Icon(Icons.Filled.Home, "") }
-                            )
-                        }
-                    }) {paddingValues ->
-                        val navController = rememberNavController()
-                        NavHost(
-                            navController = navController,
-                            startDestination = Screen.OverviewScreen.route,
-                            modifier = Modifier.padding(paddingValues)
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.OverviewScreen.route,
+                    ) {
+                        composable(
+                            route = Screen.OverviewScreen.route
                         ) {
-                            composable(
-                                route = Screen.OverviewScreen.route
-                            ) {
-                                OverviewScreen(navController)
-                            }
-                            composable(
-                                route = Screen.CalendarScreen.route
-                            ) {
-                                CalendarScreen(navController)
-                            }
+                            OverviewScreen(navController)
                         }
+                        composable(
+                            route = Screen.CalendarScreen.route
+                        ) {
+                            CalendarScreen(navController)
+                        }
+
+                        composable(
+                            route = Screen.TestScreen.route
+                        ) {
+                            TestScreen(navController)
+                        }
+
                     }
                 }
             }
--- app/src/main/java/com/example/p3project/sources/presentation/screens/Screen.kt
+++ app/src/main/java/com/example/p3project/sources/presentation/screens/Screen.kt
@@ -3,4 +3,5 @@ package com.example.p3project.sources.presentation.screens
 sealed class Screen(val route: String) {
     data object OverviewScreen: Screen("overview")
     data object CalendarScreen: Screen("calendar")
+    data object TestScreen: Screen("debug")
 }
