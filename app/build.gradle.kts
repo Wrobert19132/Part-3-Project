666e4b8f824992e7d246869e7cec265a964ad1e9
William Robertson <whr1g21@soton.ac.uk>
2024-03-15 11:58:48 +0000

    Fixed Room dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,11 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.51")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.hilt:hilt-common:1.2.0")
+    implementation("androidx.hilt:hilt-navigation:1.2.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51")
 
 
