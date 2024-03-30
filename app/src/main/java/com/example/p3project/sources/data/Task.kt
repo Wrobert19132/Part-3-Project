29b783acf379609aeaf9993e167540cf356900a9
William Robertson <whr1g21@soton.ac.uk>
2024-03-30 12:40:49 +0000

    Added date / time helper functions

--- app/src/main/java/com/example/p3project/sources/data/Task.kt
+++ app/src/main/java/com/example/p3project/sources/data/Task.kt
@@ -18,7 +18,17 @@ data class Task (
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 
+    fun isTaskDay(date: LocalDate): Boolean {
+        return daysUntilNextTaskDay(date) == 0
+    }
 
+    fun daysUntilNextTaskDay(date: LocalDate): Int {
+        return ((date.toEpochDay() - startDate.toEpochDay())  % dayInterval).toInt()
+    }
+
+    fun nextTaskDay(date: LocalDate): LocalDate {
+        return LocalDate.ofEpochDay(date.toEpochDay() + daysUntilNextTaskDay(date))
+    }
 
     companion object Limits {
         var maxNameLength: Int = 32;
