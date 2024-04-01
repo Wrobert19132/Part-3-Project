3ab487d8975aa92fd54282dfde1f570ed90e9f33
William Robertson <whr1g21@soton.ac.uk>
2024-04-01 07:34:04 +0100

    TaskCards show relative times sometimes

--- app/src/main/java/com/example/p3project/sources/presentation/screens/overview/components/TaskCard.kt
+++ app/src/main/java/com/example/p3project/sources/presentation/screens/overview/components/TaskCard.kt
@@ -3,16 +3,12 @@ package com.example.p3project.sources.presentation.screens.overview.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
@@ -24,15 +20,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.example.p3project.sources.data.Task
+import com.example.p3project.sources.data.database.Task
+import java.time.LocalDateTime
 
 @Composable
 fun TaskCard (task: Task, onClick: () -> Unit) {
+
+    val now = LocalDateTime.now()
+
     ElevatedCard (modifier = Modifier
         .fillMaxWidth()
         .defaultMinSize(minHeight = 100.dp)
         .padding(2.dp),
-        onClick = onClick
+        onClick = onClick,
           )
 
     {
@@ -49,14 +49,33 @@ fun TaskCard (task: Task, onClick: () -> Unit) {
                     style = MaterialTheme.typography.titleMedium,
                 )
 
-                // Text(
-                //    text = String.format("%02d:%02d",
-                //        task.hour, task.minute),
-                //    modifier = Modifier.fillMaxWidth(),
-                //    overflow = TextOverflow.Ellipsis,
-                //    style = MaterialTheme.typography.titleMedium,
-                //    textAlign = TextAlign.Right
-                //    )
+                if (task.isTaskDay(now.toLocalDate())) {
+                    if (task.minutesUntilTask(now) < 60) {
+                        Text(
+                            text = task.minutesUntilTask(now).toString() + " minutes away",
+                            modifier = Modifier.fillMaxWidth(),
+                            style = MaterialTheme.typography.titleMedium,
+                            color = MaterialTheme.colorScheme.tertiary,
+                            textAlign = TextAlign.Right,
+                        )
+                    } else {
+                        Text(
+                            text = String.format("%02d:%02d", task.time.hour, task.time.minute),
+                            modifier = Modifier.fillMaxWidth(),
+                            style = MaterialTheme.typography.titleMedium,
+                            color = MaterialTheme.colorScheme.tertiary,
+                            textAlign = TextAlign.Right,
+                        )
+                    }
+                } else {
+                    Text(
+                        text = task.daysUntilNextTaskDay(now.toLocalDate()).toString() + " days away",
+                        modifier = Modifier.fillMaxWidth(),
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.Right
+                    )
+                }
+
 
                 // Debug
                 Text(
