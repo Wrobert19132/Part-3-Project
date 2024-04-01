b8dd7f4a755a4390ac226bcf7239ea0774f0475c
William Robertson <whr1g21@soton.ac.uk>
2024-04-01 10:50:10 +0100

    added interrupt scheduler to Dagger + Hilt

--- app/src/main/java/com/example/p3project/di/AppModule.kt
+++ app/src/main/java/com/example/p3project/di/AppModule.kt
@@ -1,13 +1,18 @@
 package com.example.p3project.di
 
 import android.app.Application
+import android.content.Context
+import androidx.compose.ui.platform.LocalContext
 import androidx.room.Room
+import com.example.p3project.sources.data.InterruptScheduler
+import com.example.p3project.sources.data.InterruptSchedulerImpl
 import com.example.p3project.sources.data.database.TaskDatabase
 import com.example.p3project.sources.repository.TaskRepository
 import com.example.p3project.sources.repository.TaskRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -23,6 +28,12 @@ class AppModule {
             TaskDatabase::class.java,
             TaskDatabase.DATABASE_NAME
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideInterruptScheduler(@ApplicationContext context: Context): InterruptScheduler {
+        return InterruptSchedulerImpl(context)
     }
 
     @Provides
