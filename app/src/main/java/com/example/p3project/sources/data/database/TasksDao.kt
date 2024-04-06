8ca607b9dbc9b39d5853f6035cebb83be1bf0e02
William Robertson <whr1g21@soton.ac.uk>
2024-04-06 10:00:55 +0100

    PrimaryKey of added task is now returned

--- app/src/main/java/com/example/p3project/sources/data/database/TasksDao.kt
+++ app/src/main/java/com/example/p3project/sources/data/database/TasksDao.kt
@@ -9,7 +9,7 @@ import com.example.p3project.sources.data.database.Task
 @Dao
 interface TasksDao {
     @Insert(onConflict = OnConflictStrategy.ABORT)
-    suspend fun addTask(task: Task)
+    suspend fun addTask(task: Task): Long
 
     @Query("SELECT * FROM task WHERE id=:id")
     fun getTask(id: Int): Task?
