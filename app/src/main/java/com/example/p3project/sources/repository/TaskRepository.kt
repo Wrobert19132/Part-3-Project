b8add0e43b25b9c4c1b775bb5e692b3c6f2f9c12
William Robertson <whr1g21@soton.ac.uk>
2024-04-06 10:00:35 +0100

    Made getTask a suspend function

--- app/src/main/java/com/example/p3project/sources/repository/TaskRepository.kt
+++ app/src/main/java/com/example/p3project/sources/repository/TaskRepository.kt
@@ -6,7 +6,7 @@ import com.example.p3project.sources.data.database.Task;
 // and hilt
 interface TaskRepository {
     suspend fun getTasks(): List<Task>
-    fun getTask(id: Int): Task?
+    suspend fun getTask(id: Int): Task?
     suspend fun addTask(task: Task)
 
 }
