531490eb8e5fd153cd1e82a0d06570cad7ed50cb
William Robertson <whr1g21@soton.ac.uk>
2024-04-14 13:17:54 +0100

    Added task completion events to overview

--- app/src/main/java/com/example/p3project/presentation/screens/overview/OverviewViewmodel.kt
+++ app/src/main/java/com/example/p3project/presentation/screens/overview/OverviewViewmodel.kt
@@ -2,11 +2,15 @@ package com.example.p3project.presentation.screens.overview
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.p3project.domain.model.Task
 import com.example.p3project.domain.usecases.UseCases
+import com.example.p3project.domain.util.OverviewMode
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.MutableStateFlow
+import java.time.LocalDate
+import java.time.LocalTime
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,7 +19,7 @@ class OverviewViewmodel @Inject constructor(
 ): ViewModel() {
 
     public val state = MutableStateFlow(
-        OverviewState(listOf())
+        OverviewState(listOf(), OverviewMode.TodayView)
     )
 
     fun onEvent(event: OverviewEvent) {
@@ -23,10 +27,32 @@ class OverviewViewmodel @Inject constructor(
             viewModelScope.launch(Dispatchers.IO) {
                 getTasks()
             }
-        }
+        } else if (event is OverviewEvent.CompleteTask) {
+            viewModelScope.launch(Dispatchers.IO) {
+                completeTask(event.task)
+            }
+        } else if (event is OverviewEvent.UpdateViewMode) {
+            viewModelScope.launch(Dispatchers.IO) {
+                updateViewMode(event.viewMode)
+            }
+         }
+    }
+    private suspend fun completeTask(task: Task) {
+
+        useCases.completeTasksUseCase(task,
+                                      task.periodsPassed(LocalDate.now()),
+                                      LocalTime.now()
+        )
+
+        getTasks()
+    }
+
+    private suspend fun updateViewMode(mode: OverviewMode) {
+        state.value = state.value.copy(viewMode = mode)
+        getTasks()
     }
 
     private suspend fun getTasks() {
-        state.value = state.value.copy(tasks = useCases.getTasksUseCase())
+        state.value = state.value.copy(taskAndCompletions = useCases.getTaskCompletionsUseCase())
     }
 }
